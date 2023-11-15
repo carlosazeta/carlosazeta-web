070041980f4e2ee4aabe5040da2a1950d7468d1a
carlosazeta <carlosagundezdev@gmail.com>
2023-11-15 21:50:49 +0100

    Add card mobile breakpoints

--- src/components/PortfolioCard.jsx
+++ src/components/PortfolioCard.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 
 export const PortfolioCard = () => {
   return (
-    <div className='max-w-sm mx-auto bg-transparent overflow-hidden md:max-w-xl m-12'>
+    <div className='mx-6 md:mx-auto max-w-sm bg-transparent overflow-hidden md:max-w-xl m-12'>
       <div className='antialiased'>
         <div className='mt-4'>
           <div className='text-xl font-bold text-black dark:text-white'>Carlos Agúndez</div>
           <p className='text-gray-500 dark:text-gray-300 mb-6'>Frontend Developer</p>
           <p className='mt-2 text-black dark:text-white text-left'>
-            I'm a frontend developer based in Spain<br />
+            I'm a frontend developer based in Spain.<br />
+          </p>
+          <p className='mt-2 text-black dark:text-white text-left'>
             I'm interested in everything related to JavaScript, React and web design.
           </p>
         </div>
