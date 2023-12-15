f11a78d56fb008345c357561db796a50703a7b4d
carlosazeta <carlosagundezdev@gmail.com>
2023-12-15 16:50:36 +0100

    Create projects data

---
+++ src/data/projectsData.js
@@ -0,0 +1,68 @@
+/* eslint-disable comma-dangle */
+/* eslint-disable indent */
+/* eslint-disable no-tabs */
+export const projectsData = [
+	{
+		id: 1,
+		title: 'Markdown Converter',
+		stack: 'React TypeScript',
+		githubUrl: 'https://github.com/carlosazeta/markdown-converter',
+		previewUrl: 'https://markdown-converter-eosin.vercel.app/',
+	},
+	{
+		id: 2,
+		title: 'Top Tracks',
+		stack: 'React TypeScript Tailwind FramerMotion',
+		githubUrl: 'https://github.com/carlosazeta/top-songs',
+		previewUrl: 'https://top-songs-six.vercel.app/',
+	},
+	{
+		id: 3,
+		title: 'Nike Landpage',
+		stack: 'React TypeScript',
+		githubUrl: 'https://github.com/carlosazeta/nike-shop',
+		previewUrl: 'https://nike-shop-sepia.vercel.app/',
+	},
+	{
+		id: 4,
+		title: 'Book Reading List',
+		stack: 'React JavaScript',
+		githubUrl: 'https://github.com/carlosazeta/book-list',
+		previewUrl: 'https://book-list-nu-two.vercel.app/',
+	},
+	{
+		id: 5,
+		title: 'Guess Pokemon',
+		stack: 'React JavaScript',
+		githubUrl: 'https://github.com/carlosazeta/guess-pokemon',
+		previewUrl: 'https://guess-pokemon-chi.vercel.app/',
+	},
+	{
+		id: 6,
+		title: 'To do Task',
+		stack: 'React TypeScript',
+		githubUrl: 'https://github.com/carlosazeta/todo-ts',
+		previewUrl: 'https://todo-ts-brown.vercel.app/',
+	},
+	{
+		id: 7,
+		title: 'Tic Tac Toe',
+		stack: 'React JavaScript',
+		githubUrl: 'https://github.com/carlosazeta/tic-tac-toe',
+		previewUrl: 'https://tic-tac-toe-dun-five-54.vercel.app/',
+	},
+	{
+		id: 8,
+		title: 'Cat Meme Generator',
+		stack: 'React JavaScript',
+		githubUrl: 'https://github.com/carlosazeta/cat-meme-generator',
+		previewUrl: 'https://cat-meme-generator.vercel.app/',
+	},
+	{
+		id: 1,
+		title: 'Users CRUD',
+		stack: 'React TypeScript',
+		githubUrl: 'https://github.com/carlosazeta/users-crud',
+		previewUrl: 'https://github.com/carlosazeta/users-crud',
+	},
+]
