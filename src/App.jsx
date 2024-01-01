df8ae09e2ab493ff24c777394c73248b542566c7
carlosazeta <carlosagundezdev@gmail.com>
2024-01-01 14:07:41 +0100

    Fix end page margin

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App () {
   return (
     <div className='flex flex-col items-center min-h-screen bg-white text-black dark:bg-black dark:text-white cursor-crosshair'>
       <NavBar />
-      <div className='flex flex-col items-center md:items-start'>
+      <div className='flex flex-col items-center md:items-start mb-4'>
         <PortfolioCard />
         <ProjectList />
       </div>
