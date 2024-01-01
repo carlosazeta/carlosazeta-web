438b7c4c99d98c66f93d0f53b6a4082386179593
carlosazeta <carlosagundezdev@gmail.com>
2024-01-01 13:47:38 +0100

    Delete old projects table

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App () {
   return (
     <div className='flex flex-col items-center min-h-screen bg-white text-black dark:bg-black dark:text-white cursor-crosshair'>
       <NavBar />
-      <div className='flex flex-col items-'>
+      <div className='flex flex-col'>
         <PortfolioCard />
         <ProjectList />
       </div>
